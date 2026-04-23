47fb21be681e1c0f0ace9ec6d18b8338d35eb63c
Mette Karlsson <mettek_96@hotmail.com>
2026-04-24 00:57:48 +0200

    bug fixes

--- src/exceptions/InvalidMemberStatusException.java
+++ src/exceptions/InvalidMemberStatusException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class InvalidMemberStatusException extends LibraryException {
     public InvalidMemberStatusException(int memberId) {
-        super("You're not allowed to borrow books. Invalid status.");
+        super("Member #" + memberId + " is not allowed to borrow books. Invalid status.");
     }
 }
